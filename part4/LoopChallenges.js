bc44db0f1915cdf74ccda3fcb775149a541db46c
Yash Sathe <yashsathe1357@gmail.com>
2026-08-03 18:06:43 +0530

    Solve some loop problems and revise function

---
+++ part4/LoopChallenges.js
@@ -0,0 +1,87 @@
+/*
+1. Write a `while` loop that calculates the sum of all
+numbers from 1 to 5 and stores the result in a variable
+named `sum`.
+*/
+// let i = 0
+// let sum = 0
+
+// while (i <=5 ) {
+//     sum += i
+//     i++;
+// }
+// console.log(`Sum : ${sum}`);
+
+/*
+2. Write a `while` loop that counts down from 5 to 1 and
+stores the numbers in an array named `countdown`.
+*/
+// let countdown = [];
+// let i = 5;
+
+// while (i >= 1) {
+//     countdown.push(i);
+//     i--;
+// }
+// console.log(countdown);
+
+/*
+3. Write a `do while` loop that prompts a user to enter
+their favorite tea type until they enter `"stop"`.
+Store each tea type in an array named `teaCollection`.
+*/
+
+// let teaCollection = [];
+// let tea;
+
+// do {
+//   tea = prompt(`Enter your favourite tea type("stop to finish") :`);
+
+//   if (tea !== "stop") {
+//     teaCollection.push(tea);
+//   }
+// } while (tea != "stop");
+
+// console.log(teaCollection);
+
+/*
+4. Write a `do while` loop that adds numbers from 1 to 3
+and stores the result in a variable named `total`.
+*/
+
+// let total = 0;
+// let i = 0
+// do {
+//     total += i;
+//     i++;
+// } while (i <= 3);
+
+// console.log(`total : ${total}`);
+
+/*
+5. Write a `for` loop that multiplies each element in the
+array `[2, 4, 6]` by 2 and stores the results in a new
+array named `multipliedNumbers`.
+*/
+
+// let arrayElements = [2,4,6];
+// let multipliedNumbers = [];
+// for(let i = 0; i < arrayElements.length; i++){
+//     multipliedNumbers.push(arrayElements[i] * 2)
+// }
+// console.log(multipliedNumbers);
+
+/*
+6. Write a `for` loop that lists all the cities in the
+array `["Paris", "New York", "Tokyo", "London"]` and
+stores each city in a new array named `cityList`.
+*/
+
+// let arrayCities = ["Paris","New York","Tokyo","London"]
+// let cityList = []
+// for( let i =0 ; i < arrayCities.length; i++){
+//     cityList.push(arrayCities[i])
+// }
+// console.log(cityList);
+
+
